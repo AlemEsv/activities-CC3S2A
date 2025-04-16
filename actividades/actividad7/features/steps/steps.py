10860396737c2f1b71589df272b6da06006a2af0
AlemEsv <cinver.espinoza.v@uni.pe>
2025-04-16 16:15:36 -0500

    Modificar steps.py para añadir funcionalidad en ingles

--- actividades/actividad7/features/steps/steps.py
+++ actividades/actividad7/features/steps/steps.py
@@ -1,5 +1,6 @@
-from behave import given, when, then
+from behave import given, when, then, register_type
 import re
+import parse
 
 # Función para convertir palabras numéricas a números
 def convertir_palabra_a_numero(palabra):
@@ -7,29 +8,46 @@ def convertir_palabra_a_numero(palabra):
         return int(palabra)
     except ValueError:
         numeros = {
+            # spanish
             "cero": 0, "uno": 1, "una": 1, "dos": 2, "tres": 3, "cuatro": 4, "cinco": 5,
             "seis": 6, "siete": 7, "ocho": 8, "nueve": 9, "diez": 10, "once": 11,
             "doce": 12, "trece": 13, "catorce": 14, "quince": 15, "dieciséis": 16,
             "diecisiete": 17, "dieciocho": 18, "diecinueve": 19, "veinte": 20,
             "treinta": 30, "cuarenta": 40, "cincuenta": 50, "sesenta": 60, "setenta": 70,
-            "ochenta": 80, "noventa": 90, "media": 0.5
+            "ochenta": 80, "noventa": 90, "media": 0.5,
+        
+            # english
+            "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+            "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11,
+            "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16,
+            "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20,
+            "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70,
+            "eighty": 80, "ninety": 90, "half": 0.5,
         }
         return numeros.get(palabra.lower(), 0)
 
-@given('que he comido {cukes:d} pepinos')
+def parsing_texto_a_float(palabra):
+    return float(palabra)
+
+register_type(palabra=parsing_texto_a_float)
+
+@given('que he comido {cukes:palabra} pepinos')
 def step_given_eaten_cukes(context, cukes):
     context.belly.comer(cukes)
 
 @when('espero {time_description}')
 def step_when_wait_time_description(context, time_description):
     time_description = time_description.strip('"').lower()
-    time_description = time_description.replace('y', ' ')
+
+    time_description = time_description.replace('y', ' ') # spanish
+    time_description = time_description.replace('and', ' ') # english
+
     time_description = time_description.strip()
 
     if time_description == 'media hora':
         total_time_in_hours = 0.5
     else:
-        pattern = re.compile(r'(?:(\w+)\s*horas?)?\s*(?:(\w+)\s*minutos?)?\s*(?:(\w+)\s*segundos?)?')
+        pattern = re.compile(r'(?:(\w+)\s*(?:horas?|hours?))?\s*(?:(\w+)\s*(?:minutos?|minutes?))?\s*(?:(\w+)\s*(?:segundos?|seconds?))?')
         match = pattern.match(time_description)
 
         if match:
